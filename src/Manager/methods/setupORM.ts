2521ec58d6b8346bce40928b92af77ac97723e9d
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-02-16 21:13:51 -0600

    one liner

--- src/Manager/methods/setupORM.ts
+++ src/Manager/methods/setupORM.ts
@@ -4,27 +4,33 @@ const setupORM = async function(){
 
   for(let key in this.project.databases){
 
-    console.log('key is ', key)
-    // console.log('key is ', key)
+    this.orm[key] = this.ormTypes[this.project.databases[key].type].default
 
 
-    let dbType = ""
+// this.ormTypes[this.project.databases[key].type].default
 
-      switch (this.project.databases[key].type) {
-        case 'mysql': dbType = this.ormTypes.mysql.default
-          break;
-
-        case 'mongo': dbType = this.ormTypes.mongo.default
-          break;
-        default:
-      }
-
-    this.orm[key] = dbType;
-
-
-console.log('+++++++++++++++++++')
-console.log(this.orm)
-console.log('+++++++++++++++++++')
+//     console.log('key is ', key)
+//     // console.log('key is ', key)
+//
+//
+//
+//     let dbType = ""
+//
+//       switch (this.project.databases[key].type) {
+//         case 'mysql': dbType = this.ormTypes.mysql.default
+//           break;
+//
+//         case 'mongo': dbType = this.ormTypes.mongo.default
+//           break;
+//         default:
+//       }
+//
+//     this.orm[key] = dbType;
+//
+//
+// console.log('+++++++++++++++++++')
+// console.log(this.orm)
+// console.log('+++++++++++++++++++')
 
 
   }
