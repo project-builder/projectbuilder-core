2080c5e4faf8d36da1699b4c235032b11e54dc06
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-03-27 19:54:23 -0500

    maybe fix fs?

--- src/Manager/methods/setupFS.ts
+++ src/Manager/methods/setupFS.ts
@@ -2,17 +2,21 @@ const setupFS = async function(){
   this.fs = {}
 
   for(let key in this.project.fileSystems){
-    let fsType = ""
-
-      switch (this.project.fileSystems[key].type) {
-        case 'S3': fsType = this.fsTypes.s3.default
-          break;
-        default:
-      }
-
-    this.fs[key] = fsType;
-
+    this.fs[key] = this.fsTypes[this.project.fileSystems[key].type].default
   }
+
+  // for(let key in this.project.fileSystems){
+  //   let fsType = ""
+  //
+  //     switch (this.project.fileSystems[key].type) {
+  //       case 'S3': fsType = this.fsTypes.s3.default
+  //         break;
+  //       default:
+  //     }
+  //
+  //   this.fs[key] = fsType;
+  //
+  // }
 }
 
 export default setupFS
